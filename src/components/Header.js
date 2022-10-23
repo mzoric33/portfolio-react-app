0ad15537a5cd3ee456871aa38416acd146857b7e
mzoric33 <zoricmarin4@gmail.com>
2022-10-23 20:24:46 +0200

    header

---
+++ src/components/Header.js
@@ -0,0 +1,37 @@
+import React from "react";
+import classes from "./Header.module.scss";
+import { BiMenuAltRight } from "react-icons/bi";
+import { AiOutlineClose } from "react-icons/ai";
+
+const Header = () => {
+  return (
+    <header>
+      <div className={classes.header__content}>
+        <nav className={classes.header__content__nav}>
+          <h2 className={classes.header__content__logo}>
+            OSOBNI <span className={classes.orange}>PORTFOLIO</span>
+          </h2>
+          <ul>
+            <li>
+              <a href="#">NASLOVNICA</a>
+            </li>
+            <li>
+              <a href="#">O MENI</a>
+            </li>
+            <li>
+              <a href="#">PROJEKTI</a>
+            </li>
+            <li>
+              <a href="#">KONTAKT</a>
+            </li>
+          </ul>
+          <div className={classes.header__content__toggle}>
+            <BiMenuAltRight />
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
